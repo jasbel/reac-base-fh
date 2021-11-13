5c1f92a439b4dac45a0bc2686c18ee10561f499b
jasbel <asbel.elmec@gmail.com>
2021-11-13 18:29:29 -0400

    paginas lazy

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,63 +1,50 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
 } from "react-router-dom";
 import logo from "../logo.svg";
 import CounterView from "../views/CounterView";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 const Navigation = () => {
-    return (
-        <Router>
-            <div className="main-layout">
-                <nav>
-                    <img src={logo} alt="React logo" />
-                    <ul>
-                        <li>
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                                to="/"
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                                to="/about"
-                            >
-                                About
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? "nav-active" : ""
-                                }
-                                to="/users"
-                            >
-                                Users
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React logo" />
+          <ul>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? "nav-active" : "" } to="/lazy1" >
+                lazy1
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? "nav-active" : "" } to="/lazy2" >
+                lazy2
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => isActive ? "nav-active" : "" } to="/lazy3" >
+                lazy3
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-                {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-                <Routes>
-                    <Route path="/about" element={<h1>about</h1>}></Route>
-                    <Route path="/users" element={<h1>probando</h1>}></Route>
-                    <Route path="/" element={<CounterView />}></Route>
-                </Routes>
-            </div>
-        </Router>
-    );
+        <Routes>
+          <Route path="/lazy1" element={<LazyPage1 />}></Route>
+          <Route path="/lazy2" element={<LazyPage2 />}></Route>
+          <Route path="/lazy3" element={<LazyPage3 />}></Route>
+          <Route path="/counter" element={<CounterView />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default Navigation;
